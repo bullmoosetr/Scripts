316e6f3e305d6d33ab8bfb295fe6f4d19c03c3a0
bullmoosetr <ericlaff16@gmail.com>
2017-11-12 19:50:12 -0500

    Updated

--- BaacklogCountDC3toNJ2v4.ps1
+++ BaacklogCountDC3toNJ2v4.ps1
@@ -6,11 +6,11 @@ function BacklogCount {
         $VerbosePreference = 'Continue'
         $Date = Get-Date
         Add-Content \\ideal.prod.local\STORAGE\~NOC\DFSBacklogCheckLog\BacklogCountDC3toNJ2.txt "$Date" 
-        $VerboseMessage = (Get-DfsrBacklog -GroupName "*" -SourceComputerName $env:COMPUTERNAME -DestinationComputerName "DFS1NJ2US03" -Verbose 4>&1)
+        $VerboseMessage = (Get-DfsrBacklog -GroupName "*" -SourceComputerName $env:COMPUTERNAME -DestinationComputerName "MYSERVER" -Verbose 4>&1)
         if($VerboseMessage -like "*The replicated folder has a backlog of files*")
         {
             for ($i = 0; $i -lt 5; $i++) {
-               $VerboseMessage = (Get-DfsrBacklog -GroupName "*" -SourceComputerName $env:COMPUTERNAME -DestinationComputerName "DFS1NJ2US03" -Verbose 4>&1) 
+               $VerboseMessage = (Get-DfsrBacklog -GroupName "*" -SourceComputerName $env:COMPUTERNAME -DestinationComputerName "MYSERVER" -Verbose 4>&1) 
             }
         $outputString = $VerboseMessage | Out-String
         $arr = @($outputString)
