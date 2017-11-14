efdb1c344e6f698845c7347e13d7a74dc39ea2b3
bullmoosetr <ericlaff16@gmail.com>
2017-11-14 11:42:42 -0500

    Update feedparser.ps1

--- feedparser.ps1
+++ feedparser.ps1
@@ -1,4 +1,4 @@
 ﻿#Grabs the content of the xml config
-[xml]$CasaFeed = Get-Content C:\Users\lafferic\Desktop\Test-Output-8.xml
+[xml]$Feed = Get-Content C:\Users\lafferic\Desktop\Test-Output-8.xml
 #Parses the XML file, then pipes the sections to the foreach loop and dumps these specific sections of information. last dumps to a .txt file
-$CasaFeed.configuration.FileTransferFeeds | % {$_.FileTransferJob.Tasks.Task.Steps.Step}  | Out-File C:\Users\lafferic\Desktop\MINT08-Feed-INFO.txt
+$Feed.configuration.FileTransferFeeds | % {$_.FileTransferJob.Tasks.Task.Steps.Step}  | Out-File C:\Users\lafferic\Desktop\Feed-INFO.txt
