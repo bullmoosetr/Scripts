383c59aeef6284cf72e5e6a46558f9aebfdfe981
bullmoosetr <ericlaff16@gmail.com>
2018-06-26 10:26:47 -0400

    Update CasaFeedParser.ps1

--- CasaFeedParser.ps1
+++ CasaFeedParser.ps1
@@ -1,5 +1,5 @@
 ﻿$servers = 'Server'.Split(',')#Add servers to tuple 
 foreach ($server in $servers){
 Write-Host $server
-$result = Invoke-Command -ComputerName $server -ScriptBlock {Get-Content -Path "D:\Temp\NOC TEST\Test.txt" | Where-Object {$_ -like '**'} } 
+$result = Invoke-Command -ComputerName $server -ScriptBlock {Get-Content -Path "" | Where-Object {$_ -like '**'} } 
 }
